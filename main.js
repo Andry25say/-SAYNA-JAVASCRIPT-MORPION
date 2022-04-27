f18d0d1f0549c926ba9486e8a2027b56a3d8669c
Andry <andryrichard2022@gmail.com>
2022-04-27 16:34:09 +0300

    first commit morpion

---
+++ main.js
@@ -0,0 +1,87 @@
+(function() {
+
+    // Récupération des cases à clicker
+    const items = document.getElementsByClassName('grid-item');
+    const tour = document.querySelector(".cpu-turn")
+
+let jeuActive = true;
+let joueurActif = "X";
+let etatDuJeux = ["","","","","","","","",""];
+
+const conditionDeVictoire =
+[ 
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6],
+
+]
+const tourJoueurs =() =>`c'est au tour du joueur ${joueurActif}`;
+
+tour.innerHTML = tourJoueurs();
+
+let cases = document.querySelectorAll(".grid-item");
+
+
+    cases.forEach(caz =>caz.addEventListener("click",choiseCase));
+
+function choiseCase() {
+  const indexCase = this.id;
+  const chaqueCase = document.getElementById(indexCase);
+
+  console.log(indexCase)
+  if (etatDuJeux[[...cases].indexOf(this)] !== "" || !jeuActive){
+      return;
+  }
+  etatDuJeux[[...cases].indexOf(this)] = joueurActif;
+  chaqueCase.innerHTML = joueurActif;
+  
+  verificationVictoire()
+    }
+function verificationVictoire(){
+    let tourGagnant = false;
+    for(let conditionVic of conditionDeVictoire){
+        let valeur1 = etatDuJeux[conditionVic[0]]
+        let valeur2 = etatDuJeux[conditionVic[1]]
+        let valeur3 = etatDuJeux[conditionVic[2]]
+
+        if(valeur1 === "" || valeur2 === "" || valeur3 === ""){
+            continue;
+        }
+        if(valeur1 === valeur2 && valeur2 === valeur3){
+            tourGagnant = true;
+            break;
+        }
+    }
+    if(tourGagnant){
+        let afficheVictoire = document.querySelector(".layer2");
+            afficheVictoire.style.display="block";
+        let fermer = document.querySelector(".end-game");
+            fermer.addEventListener("click",() => {afficheVictoire.style.display="none"});
+    }
+    if(!etatDuJeux.includes("")){
+        let afficheNul = document.querySelector(".layer3");
+        afficheNul.style.display="block";
+        let recommencer = document.querySelector(".draw");
+            recommencer.addEventListener("click",() => {afficheNul.style.display="none"});
+    
+    }
+    joueurActif = joueurActif === "X"? "O": "X";
+    tour.innerHTML = tourJoueurs();
+}
+
+
+
+    const pop1 = document.querySelector(".layer");
+    let choix1 = document.querySelector(".choose1");
+    let choix2 = document.querySelector(".choose2");
+
+        choix1.addEventListener("click",() => {pop1.style.display = "none"});
+        choix2.addEventListener("click",() => {pop1.style.display = "none"});
+
+})();
+            
